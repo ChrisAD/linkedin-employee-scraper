092653288029e7f3d34b7f1d559d9825ec34fa7d
VegarLH <vegarlh@gmail.com>
2018-12-10 15:15:31 +0100

    Add title and some minor changes
    This change adds title to the employee output.
    Also:
     - Added debug flag and some debug output-
     - Added some comments.
     - Fixed a missing semicolon on line 31

--- linkedin-employee-extract.js
+++ linkedin-employee-extract.js
@@ -1,59 +1,95 @@
 // ==UserScript==
 // @name         LinkedIn employee extractor
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      1
 // @author       You
 // @include  https://www.linkedin.com/search/results/people/?facetCurrentCompany=*
-// @version  1
-// @connect  linkedin.com
 // @run-at   document-end
-// @grant    GM_getValue
-// @grant    GM_setValue
-// @grant    GM_deleteValue
+// @grant GM_getValue
+// @grant GM_setValue
+// @grant GM_deleteValue
 // @require  https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.3.1.min.js
 // @icon https://cdn2.hubspot.net/hubfs/4452610/favico-1.png
 // ==/UserScript==
+
+//Set to true to turn on debugging
+var debug = false;
+
 (function() {
     'use strict';
 
+if(debug) console.log("Greasemonkey-linkedin script loaded");
+
 var scroll = 250;
-var all = [];
-function printnames() {
-	setTimeout(function() {
-		$(".actor-name").map(function() {
-			all.push(this.innerHTML);
-		});
-		window.scrollTo(0,scroll);
-		if(Math.ceil($(window).scrollTop() + $(window).height()) == $(document).height()) {
-            //Reset the scroll value. It will be reset anyways by the userscript reloading, but nice to have if needed to run this within browser console
-			scroll = 0;
-            //Get values from userscript
-            var allEmps = JSON.parse (GM_getValue ("employees",  null) )
-            var joined = all.concat(allEmps); //Join the arrays
-            //Save values in userscript
-            //console.log("Done with page, pushing this into userscript storage: " + JSON.stringify (joined))
-            GM_setValue ("employees",  JSON.stringify (joined) );
-			if($(".next").length > 0) {
-			     //Parse URL and load new URL instead of clicking the Next button. Clicking next is normally acceptable, but LinkedIn is weird, and doesn't properly reload the URL, hence doesn't trigger the userscript
-                 var url = window.location.href;
-				 var search = "page=";
-				 var changeIndex = url.indexOf("page=")+search.length;
-				 var newPage = parseInt(url.substring(changeIndex, changeIndex+1)) + 1;
-				 url = url.substring(0,url.length -1) + newPage;
-                 //console.log("Loading url: " + url);
-				 window.location.replace(url);
-			} else {
-                //Get only uniques since this is final execution of the script
-                var unique = joined.filter((v, i, a) => a.indexOf(v) === i);
-                console.log("Collected " + unique.length + " employees...");
-                console.log(unique.join("\n"));
-                GM_deleteValue("employees");
-            };
-		} else {
-		 	scroll += 350;
-			printnames();
-		}
-	}, 1000);
-};
-printnames();
+var allEmps = [];
+var allTitles = [];
+    function printnames() {
+        if(debug) console.log("Entering printnames()");
+        setTimeout(function() {
+            $(".actor-name").map(function() {
+                allEmps.push(this.innerHTML);
+            });
+            $(".subline-level-1").map(function() {
+                allTitles.push(this.children[0].innerHTML);
+            });
+
+            window.scrollTo(0,scroll);
+
+            //Look for end of page
+            if(Math.ceil($(window).scrollTop() + $(window).height()) == $(document).height()) {
+                if(debug) console.log("End of current page matched");
+                //Reset the scroll value. It will be reset anyways by the userscript reloading, but nice to have if needed to run this within browser console
+                scroll = 0;
+                //Get previously stored values from userscript
+                var allEmpsPrev = JSON.parse(GM_getValue("employees",null)); if(allEmpsPrev == null) allEmpsPrev = [];
+                var allTitlesPrev = JSON.parse(GM_getValue("titles",null)); if(allTitlesPrev == null) allTitlesPrev = [];
+                //Combine previous results with current
+                var joinedEmps = allEmps.concat(allEmpsPrev);
+                var joinedTitles = allTitles.concat(allTitlesPrev);
+                //Save values in userscript
+                if(debug) console.log("Done with page, pushing Employees into userscript storage: " + JSON.stringify (joinedEmps));
+                if(debug) console.log("Done with page, pushing Titles into userscript storage: " + JSON.stringify (joinedTitles));
+                GM_setValue("employees",JSON.stringify(joinedEmps) );
+                GM_setValue("titles",JSON.stringify(joinedTitles));
+                if($(".next").length > 0) {
+                    //Parse URL and load new URL instead of clicking the Next button. Clicking next is normally acceptable, but LinkedIn is weird, and doesn't properly reload the URL, hence doesn't trigger the userscript
+                    var url = window.location.href;
+                    if(debug) console.log("url orig: " + url);
+                    var search = "page=";
+                    //Make sure page is present. allows script to run from https://www.linkedin.com/search/results/people/?facetCurrentCompany=[<id>]
+                    if(url.indexOf("page=") == -1) {
+                        url = url+"&page=0";
+                        changeIndex = url.indexOf("page=")+search.length;
+                    }
+                    var changeIndex = url.indexOf("page=")+search.length;
+                    var newPage = parseInt(url.substring(changeIndex, changeIndex+1)) + 1;
+                    url = url.substring(0,url.length -1) + newPage;
+                    if(debug) console.log("Loading url: " + url);
+                    window.location.replace(url);
+                } else {
+                    //Combine names and titles
+                    var combined = [];
+                    for(var i=0; i < joinedEmps.length; i++) {
+                        combined.push(joinedEmps[i] + "|" + joinedTitles[i]);
+                    }
+                    if(debug) console.log(combined);
+
+                    //Get only uniques since this is final execution of the script
+                    var uniqueAll = combined.filter((v, i, a) => a.indexOf(v) === i);
+
+                    console.log("Collected " + uniqueAll.length + " employees and titles..");
+                    for(var j=0; j < uniqueAll.length; j++) {
+                        console.log(uniqueAll[j] + "\n");
+                    }
+                    GM_deleteValue("employees");
+                    GM_deleteValue("titles");
+                };
+            } else {
+                if(debug) console.log("Scrolling..");
+                scroll += 350;
+                printnames();
+            }
+        }, 1000);
+    };
+    printnames();
 })();
